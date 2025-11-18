2be23ac1cece6bf7573461e7897a02a2e6e748a3
Amaan Pathan <amanpathan348@gmail.com>
2025-11-18 16:12:44 +0530

    cronJobs, pdfGenerator, uitility-Modules

---
+++ sprint 1/uitility.modules/cron.cjs
@@ -0,0 +1,21 @@
+const express = require('express')
+const cron = require('node-cron');
+
+cron.schedule('*/5 * * * *', () => {
+  console.log('running a task every 5 minute');
+});
+
+
+
+
+
+//  # ┌────────────── second (optional)
+//  # │ ┌──────────── minute
+//  # │ │ ┌────────── hour
+//  # │ │ │ ┌──────── day of month
+//  # │ │ │ │ ┌────── month
+//  # │ │ │ │ │ ┌──── day of week
+//  # │ │ │ │ │ │
+//  # │ │ │ │ │ │
+//  # * * * * * *
+ 
